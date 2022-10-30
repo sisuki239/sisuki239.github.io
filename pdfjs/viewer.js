079464d16e2151f42d5b687466526d21b5a653c2
Alyosha <21anh.nt@vinuni.edu.vn>
2022-10-30 23:39:42 +0700

    test scaling

--- pdfjs/viewer.js
+++ pdfjs/viewer.js
@@ -45,7 +45,7 @@ var mozL10n = document.mozL10n || document.webL10n;
 
 
 var CSS_UNITS = 96.0 / 72.0;
-var DEFAULT_SCALE_VALUE = 'page-fit';
+var DEFAULT_SCALE_VALUE = 'actual-size';
 var DEFAULT_SCALE = 1.0;
 var UNKNOWN_SCALE = 0;
 var MAX_AUTO_SCALE = 1.25;
